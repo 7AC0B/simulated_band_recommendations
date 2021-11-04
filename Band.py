dd38801f27825c8e2f1c389911ddbff294bada22
7AC0B <jacobethomsen@gmail.com>
2021-11-04 22:58:24 +0000

    update 04/11/2021

--- Band.py
+++ Band.py
@@ -5,12 +5,16 @@ import pandas as pd
 import copy
 import plotly.graph_objs as go
 import plotly.express as px
+import randomname
+import band_sim_functions as bsim
+
 
 
 
 class Band:
     def __init__(self, id, genre_clusters, band_deviation_from_genre, all_band_positionings, quietly=True):
         self.id = id
+        self.name = randomname.get_name().replace("-", " ").title()
         # taste attributes
         self.genre = random.choice(genre_clusters)
         self.set_style_positioning(all_band_positionings, band_deviation_from_genre)
@@ -22,9 +26,10 @@ class Band:
 
     def set_style_positioning(self, all_band_positionings, band_deviation_from_genre):
         all_band_positionings[0].append(self.id)
+        all_band_positionings[1].append(self.name)
 
         self.style_positioning = []
-        i = 1
+        i = 2
         for genre_style_point in self.genre:
 
             trying = True
@@ -45,10 +50,10 @@ class Band:
         self.style_positioning = tuple(self.style_positioning)
 
     def get_nearest_neighbor(self, all_band_positionings):
-        train_df = all_band_positionings[all_band_positionings[0] != self.id] # self is omitted to avoid match with self
+        train_df = all_band_positionings[all_band_positionings["id"] != self.id] # self is omitted to avoid match with self
 
-        X = train_df.iloc[:, 1:train_df.shape[1]] # features of training set (i.e. columns 1 to last column)
-        y = train_df.iloc[:, 0] # first column containing band ids
+        X = np.array(train_df.iloc[:, 2:train_df.shape[1]]).tolist() # features of training set (i.e. columns 1 to last column)
+        y = np.array(train_df["id"]).tolist() # first column containing band ids
 
         knn = KNeighborsClassifier(n_neighbors=1)
         knn.fit(X, y)
@@ -56,7 +61,7 @@ class Band:
 
         return prediction[0] # [0] because it returns an array e.g. array([123])
 
-    def prepare_data_for_charting(self, all_band_positionings, genre_clusters):
+    def prepare_data_for_charting(self, all_band_positionings, genre_clusters, style_dimensions):
         df = copy.deepcopy(all_band_positionings)
 
         df['type'] = 'band'
@@ -64,78 +69,78 @@ class Band:
         genre_clusters_to_chart = copy.deepcopy(genre_clusters)
         for i in range(0, len(genre_clusters_to_chart)):
             genre_clusters_to_chart[i].insert(0, f'genre_{i+1}')
+            genre_clusters_to_chart[i].insert(1, f'NA')
 
         df_genres = pd.DataFrame(genre_clusters_to_chart)
+        df_genres = bsim.set_column_names(df_genres, style_dimensions)
         df_genres['type'] = "genre"
 
         df = df.append(df_genres)
 
         return df
 
-    def plot_nearest_neighbor(self, all_band_positionings, all_bands, genre_clusters):
-        df = self.prepare_data_for_charting(all_band_positionings, genre_clusters)
-
-        # Add knn information
-        self_position = self.style_positioning
-        target_position = all_bands[self.get_nearest_neighbor(all_band_positionings)].style_positioning
-
-        coords_for_line = list(zip(self_position, target_position))
-
-        style_dimensions = df.shape[1]-2
-
-        # Plot data
-        if style_dimensions == 2: # 2 dimensions
-            fig = px.scatter(df, x=1, y=2, color="type", hover_data=[0])
-            fig.add_trace(go.Scatter(x = coords_for_line[0], y = coords_for_line[1]))
-
-        elif style_dimensions == 3: # 3 dimensions
-            print('preparing fig')
-            fig = px.scatter_3d(df, x=1, y=2, z=3, color="type")
-            fig.add_trace(go.Scatter3d(x = coords_for_line[0], y = coords_for_line[1], z = coords_for_line[2]))
-
-        else:
-            print(f'Chart only supported for 2 or 3 style dimensions, not {style_dimensions}.')
-
-        fig.show()
-
     def create_playlist(self, playlist_length, all_band_positionings, all_bands):
         bands_to_consider = copy.deepcopy(all_band_positionings)
+        bands_to_consider = bands_to_consider[bands_to_consider["id"] != self.id]
         current_band = self
 
         playlist_bands = [self]
         for i in range(0, playlist_length):
-            next_band_id = self.get_nearest_neighbor(bands_to_consider)
+            next_band_id = self.get_nearest_neighbor(bands_to_consider) # change self to current_band for exploration playlist and
             next_band = all_bands[next_band_id]
             playlist_bands.append(next_band)
 
-            bands_to_consider = bands_to_consider[bands_to_consider[0] != current_band.id]
+            bands_to_consider = bands_to_consider[bands_to_consider["id"] != next_band.id] # change next_band to current_band for exploration playlist
 
-            current_band = all_bands[next_band_id]
+            current_band = next_band #all_bands[next_band_id]
 
         return playlist_bands
 
-    def plot_playlist(self, playlist_length, all_band_positionings, all_bands, genre_clusters):
+    def plot_playlist(self, playlist_length, all_band_positionings, all_bands, genre_clusters, style_dimensions):
         playlist_bands = self.create_playlist(playlist_length, all_band_positionings, all_bands)
 
-        playlist_positionings = [band.style_positioning for band in  playlist_bands]
+        # playlist_positionings = [band.style_positioning for band in playlist_bands]
 
-        df = self.prepare_data_for_charting(all_band_positionings, genre_clusters)
+        df = self.prepare_data_for_charting(all_band_positionings, genre_clusters, style_dimensions)
 
-        coords_for_line = np.array(playlist_positionings).T.tolist()
-
-        style_dimensions = df.shape[1]-2
+        # coords_for_line = np.array(playlist_positionings).T.tolist()
 
         # Plot data
         if style_dimensions == 2: # 2 dimensions
-            fig = px.scatter(df, x=1, y=2, color="type", hover_data=[0])
-            fig.add_trace(go.Scatter(x = coords_for_line[0], y = coords_for_line[1]))
+            fig = px.scatter(df, x="x", y="y", color="type", hover_data=["band", "x", "y", "id"], width = 650, height = 650)
+            for i in range(0,playlist_length):
+                new_trace = np.array((
+                    # playlist_bands[i].id,
+                    self.style_positioning, 
+                    playlist_bands[i].style_positioning
+                    # playlist_bands[i].name
+                    )).T.tolist()
+
+                fig.add_trace(go.Scatter(x = new_trace[0], y = new_trace[1], line = dict(color="SeaGreen", width = 1)))
+
+            axis_max = max(df["x"].max(), df["y"].max()) + 5
+            axis_min = min(df["x"].min(), df["y"].min()) - 5
+            fig.update_xaxes(range=[axis_min, axis_max])
+            fig.update_yaxes(range=[axis_min, axis_max])
+
 
         elif style_dimensions == 3: # 3 dimensions
             print('preparing fig')
-            fig = px.scatter_3d(df, x=1, y=2, z=3, color="type", hover_data=[0])
-            fig.add_trace(go.Scatter3d(x = coords_for_line[0], y = coords_for_line[1], z = coords_for_line[2]))
+            axis_max = max(df["x"].max(), df["y"].max(), df["z"].max()) + 5
+            axis_min = min(df["x"].min(), df["y"].min(), df["z"].min()) - 5
+            fig = px.scatter_3d(df, x="x", y="y", z="z", color="type", hover_data=["band", "x", "y", "id"], range_x=[axis_min, axis_max], range_y=[axis_min, axis_max], range_z=[axis_min, axis_max])
+
+            for i in range(0,playlist_length):
+                new_trace = np.array((self.style_positioning, playlist_bands[i].style_positioning)).T.tolist()
+                fig.add_trace(go.Scatter3d(x = new_trace[0], y = new_trace[1], z = new_trace[2], line = dict(color="green", width = 1)))
+
+            fig.update_traces(
+                marker=dict(size = 5)
+                )
 
         else:
             print(f'Chart only supported for 2 or 3 style dimensions, not {style_dimensions}.')
+
+        fig.update_layout(showlegend=False)
 
         fig.show()
--- band_sim_functions.py
+++ band_sim_functions.py
@@ -22,14 +22,17 @@ def generate_random_cluster_locations(dimensions = 3, cluster_count = 4):
 
 def plot_all_band_positions(all_band_positionings, genre_clusters, style_dimensions, print_table = False):
     # Prepare data
-    df = pd.DataFrame(all_band_positionings).transpose()
+    df = all_band_positionings
     df['type'] = 'band'
 
     genre_clusters_to_chart = copy.deepcopy(genre_clusters)
     for i in range(0, len(genre_clusters_to_chart)):
         genre_clusters_to_chart[i].insert(0, f'genre_{i+1}')
+        genre_clusters_to_chart[i].insert(1, f'NA')
+
 
     df_genres = pd.DataFrame(genre_clusters_to_chart)
+    df_genres = set_column_names(df_genres, style_dimensions)
     df_genres['type'] = "genre"
 
     df = df.append(df_genres)
@@ -39,13 +42,27 @@ def plot_all_band_positions(all_band_positionings, genre_clusters, style_dimensi
     
     # Plot data
     if style_dimensions == 2:
-        fig = px.scatter(df, x=1, y=2, color="type", hover_data=[0])
+        fig = px.scatter(df, x="x", y="y", color="type", hover_data=["band", "x", "y", "id"])
     elif style_dimensions == 3:
-        fig = px.scatter_3d(df, x=1, y=2, z=3, color="type")
+        fig = px.scatter_3d(df, x="x", y="y", z="z", hover_data=["band", "x", "y", "z", "id"], color="type")
     else:
         print(f'Chart only supported for 2 or 3 style dimensions, not {style_dimensions}.')
 
+    fig.update_traces(
+        marker=dict(size = 5)
+    )
+
     fig.show()
+
+def set_column_names(df, style_dimensions):
+    if style_dimensions == 2:
+        df.columns = ["id", "band", "x", "y"]
+    elif style_dimensions == 3:
+        df.columns = ["id", "band", "x", "y", "z"]
+    else:
+        pass
+    
+    return df
 
 #### HELPERS ####
 
